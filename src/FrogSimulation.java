aeba374ee4e9cefe043515d730012a18c7d8f819
Cusican33 <Yarielp12@nycstudents.net>
2025-11-14 12:34:43 -0500

    starting work

---
+++ src/FrogSimulation.java
@@ -0,0 +1,14 @@
+public class FrogSimulation
+{
+    private int goalDistance;
+    private int maxHops;
+    public FrogSimulation(int dist, int numHops)
+    {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+    private int hopDistance()
+    {
+
+    }
+}
